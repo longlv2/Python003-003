6dbf116731f308edec70d385df2f1bc0afbc301f
码云 <lively432@126.com>
2020-08-23 08:49:28 +0800

    add zuoye1a.py

--- week01/spider_miaoyan1.py
+++ week01/spider_miaoyan1.py
@@ -8,10 +8,11 @@ import time
 
 # 返回bs对象
 def get_url(myurl):
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    #user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
     header = {
         'user-agent': user_agent,
-        'cookie': 'uuid_n_v=v1; uuid=78016D70E35B11EA94345B95E7867004FCBF7E861048496DB7EE22322EC1B5DE; _csrf=dcc0db631d205cbef8d23d4b4ddccfd8cf56b1a14d698e165da6a9b4bffc7555; _lxsdk_cuid=174096d6d34c8-0792fd5fb08f1-c373667-144000-174096d6d34c8; _lxsdk=78016D70E35B11EA94345B95E7867004FCBF7E861048496DB7EE22322EC1B5DE; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1597886000,1597973346,1597979353; mojo-uuid=2b31650890aacf17f613a054490eee34; mojo-session-id={"id":"4a73a990d9b7d50ca4070bda99482720","time":1597979353449}; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1597979362; __mta=144137098.1597979353431.1597979353431.1597979362238.2; mojo-trace-id=3; _lxsdk_s=1740efddf97-446-141-dd7%7C%7C7'
+        'cookie': '__mta=251100857.1597935083958.1597964713055.1597964729537.5; uuid_n_v=v1; uuid=9C315DC0E2F411EA984097C6069B5E1D8D7B52490C4A4D1B93B1236D0E8CCF5D; mojo-uuid=a6f7e7b3b36710a2b6f92555dd7bd6a2; _lxsdk_cuid=1740c5a6146c8-007f09683322e7-e323069-144000-1740c5a6146c8; _lxsdk=9C315DC0E2F411EA984097C6069B5E1D8D7B52490C4A4D1B93B1236D0E8CCF5D; _csrf=b7e502bce25702f79dc2d12e04ed5b51afd4b27775e0b04484970525483238a7; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1597935084,1597935283,1597963567,1598137889; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1598137889; mojo-session-id={"id":"a30c29e909b4a79214c212c46e2b84a6","time":1598137888612}; __mta=251100857.1597935083958.1597964729537.1598137892525.6; mojo-trace-id=2; _lxsdk_s=1741870efee-704-033-9ea%7C%7C3'
         }
     resopnse = requests.get(myurl, headers=header)
     bs_info = bs(resopnse.text, 'html.parser')
